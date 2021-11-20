528c5a7a23d29c192192d9f87b108804038c3d4f
iiaii <jhun9409@naver.com>
2021-11-21 00:29:10 +0900

    feat: @ExceptionHandler 적용

---
+++ src/main/java/me/iiaii/springexception/exception/ErrorResult.java
@@ -0,0 +1,13 @@
+package me.iiaii.springexception.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+
+    private String code;
+    private String message;
+
+}
